2e55d0703885c9916e8441ce21e07cfc54126fa1
CodeLabTF <ft_rg_zen@proton.me>
2024-01-02 19:24:14 +0200

    No1

---
+++ backups/%home%ftzen%Desktop%CodeLabD001%dojo-app%dojo-blog%src%components%Navbar.js
@@ -0,0 +1,18 @@
+const Home  = () => {
+  return (
+
+    <div className="navbar">
+      
+    <h1> The Dojo Blog </h1>
+    <div>
+     <a href=""> Home </a> 
+     <a href="/create"> New Blog      </a>  
+	
+     </div>
+    </div>
+    ); 
+}
+ 
+export default Home;
+
+
